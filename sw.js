cfee875a711d74ed74e6580e521c558983a354e5
ruangbelajarjapan1-cloud <ruangbelajarjapan1@gmail.com>
2026-08-10 22:01:59 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 // server (tidak di-cache) — cuma "kerangka" app ini yang di-cache.
 // ============================================================
 
-const CACHE_NAME = 'dulur-shell-v2';
+const CACHE_NAME = 'dulur-shell-v3';
 
 const FILE_KERANGKA = [
   './index.html',
